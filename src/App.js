37819e00fd11d49c63340bc1ea4be7efa6a4b6c3
sih3rron <siherron+github@gmail.com>
2022-09-22 10:56:31 +0100

    1 file modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 import Banner from '../components/Banner';
 import Section from '../components/Section';
 import './style.css';
@@ -6,6 +7,7 @@ import './style.css';
 export default function App() {
   return (
     <div>
+      <Navbar />
       <Banner />
       <Section />
     </div>

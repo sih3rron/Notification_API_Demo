f3ac3e55444c23f5629edc60563772df095b6ec0
sih3rron <siherron+github@gmail.com>
2022-09-22 09:31:50 +0100

    1 binaries uploaded

--- components/Banner.js
+++ components/Banner.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../components/Button';
 
 export default function Banner() {
+  const [show, setShow] = useState(true);
+  const showBanner = () => {
+    setShow((current) => !current);
+  };
+
   let permission = 'default';
   switch (Notification.permission) {
     case 'denied':
@@ -13,15 +18,20 @@ export default function Banner() {
     default:
       permission = 'default';
   }
-  console.log(Notification.permission);
+
   return (
-    <div className="mx-auto bg-gray-900 p-4">
-      <div className="flex items-center justify-between flex-wrap">
-        <div className="flex w-0 flex-1 items-center text-white">
-          This is my Banner.
+    <div>
+      {show === false ? null : (
+        <div className="mx-auto bg-gray-900 p-4">
+          <div className="flex items-center justify-between flex-wrap">
+            <div className="flex w-0 flex-1 items-center text-white">
+              This is my Banner.
+            </div>
+            <Button text={permission} />
+            <div className="times" onClick={showBanner}></div>
+          </div>
         </div>
-        <Button text={permission} />
-      </div>
+      )}
     </div>
   );
 }
--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Banner from '../components/Banner';
 import Section from '../components/Section';
-
 import './style.css';
 
 export default function App() {
